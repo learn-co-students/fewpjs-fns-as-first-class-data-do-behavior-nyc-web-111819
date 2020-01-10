cc9d2948fe9eba4a5fb1d6489272a30f4f7e204b
Edward Castillo <castilloedwarda@gmail.com>
2020-01-10 10:55:15 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timestring) {
+  timestring = parseInt(timestring)
+  if (timestring > 17) {
+    return "Good Evening"
+  } else if (timestring > 12){
+    return "Good Afternoon"
+  } else {
+    return "Good Morning"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  const timeString = document.getElementById('greeting')
+  timeString.innerText = str
+}
